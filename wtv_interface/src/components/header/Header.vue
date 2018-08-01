9c87665ac8a9feee896fb265c6b1f875781146a4
maxoon <desfouxmaxime@gmail.com>
2018-08-01 15:27:30 +0200

    add main components

---
+++ wtv_interface/src/components/header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+     header
+    </header>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+              
+            }
+        },
+        methods:{
+     
+        }
+    }
+</script>
+
+<style scoped>
+header{
+    height: 250px;
+}
+</style>
