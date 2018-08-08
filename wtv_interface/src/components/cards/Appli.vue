d2490e8e7b2c3946098c9b550e8614d71bf8ed4b
Antoine <antoinepoppe@hotmail.fr>
2018-08-08 16:13:26 +0200

    pre modale

--- wtv_interface/src/components/cards/Appli.vue
+++ wtv_interface/src/components/cards/Appli.vue
@@ -1,13 +1,21 @@
 <template>
-    
+    <div class="backAppli">
+    </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
-<style lang=<style lang="less">
-
+<style lang="less" >
+.backAppli {
+  //border : solid 5px black;
+  height: 25%;
+  width: 50%;
+  margin: 10% 10% 0% 10%;
+  background: url("/imgs/reca.jpg");
+  background-repeat: no-repeat center fixed;
+  background-size: cover;
+  background-clip: padding-box;
+}
 </style>
