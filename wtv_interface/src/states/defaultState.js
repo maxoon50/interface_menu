2450b30ada95687d6bfe1c80357052e051a63e25
Marlène Etienne <marlene.etienne@hotmail.fr>
2018-09-04 16:44:28 +0200

    fix main changed offline content

---
+++ wtv_interface/src/states/defaultState.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+export const defaultState =new Vue({
+    data: {
+        contents:[
+            {
+                'title': 'Offline',
+                'img': 'offline.png',
+                'id': 850
+            },
+            {
+                'title': 'Offline',
+                'img': 'offline.png',
+                'id': 851
+            },
+            {
+                'title': 'Offline',
+                'img': 'offline.png',
+                'id': 852
+            },
+
+        ]
+    },
+    watch:{
+
+    }
+})
