9a37414d30475b1e6e79a594a8026291be046a85
Unknown <antoinepoppe@hotmail.fr>
2018-09-04 14:53:28 +0200

    add : video preview

--- wtv_interface/src/components/cards/Channel.vue
+++ wtv_interface/src/components/cards/Channel.vue
@@ -2,6 +2,9 @@
     <transition>
     <div class="globalChan" :class="{focusborder: focused}">
         <div class="view" :style="style">
+        <video class="vid"  :class="{hide: !playVideo}" ref="video">
+        <source v-bind:src='getVideo()'  >
+        </video>
         </div>
         <div class="sub color-bg-sub">
             {{content.title}}
@@ -11,42 +14,67 @@
 </template>
 
 <script>
-    import { mixinEltWithoutChild } from "../../mixins/mixinEltWithoutChild";
+import { mixinEltWithoutChild } from "../../mixins/mixinEltWithoutChild";
 
-    export default {
-        props: ['content'],
-        mixins: [ mixinEltWithoutChild ],
-        created() {
-
-        },
-        methods:{
-          animate(){
-              this.$emit('focusChange');
-          }
-        },
-        computed: {
-            style() {
-                return 'background-image: url(/imgs/' + this.content.img + ')';
-            }
-        },
-        mounted(){
-        }
+export default {
+  props: ["content"],
+  data() {
+    return {
+      timeOut: null,
+      playVideo: false
     };
+  },
+  mixins: [mixinEltWithoutChild],
+  methods: {
+    isFocus: function() {
+      this.focused = true;
+      this.timeOut = setTimeout(this.playVid, 1500);
+    },
+    removeFocus: function() {
+      clearTimeout(this.timeOut);
+      this.focused = false;
+      this.stopVid();
+    },
+    getImg() {
+      return "http://localhost:8081/imgs/" + this.content.img;
+    },
+    getVideo() {
+      return "http://localhost:8081/videos/" + this.content.video;
+    },
+    playVid() {
+      this.playVideo = true;
+      this.$refs.video.play();
+    },
+    stopVid() {
+      this.playVideo = false;
+      this.$refs.video.pause();
+    }
+  },
+  computed: {
+    style() {
+      return "background-image: url(/imgs/" + this.content.img + ")";
+    }
+  },
+  mounted() {}
+};
 </script>
 
 <style lang="less">
-    @import '~normalize.css';
-    @import '../../assets/style/style.less';
+@import "~normalize.css";
+@import "../../assets/style/style.less";
 
-    .globalChan {
-        //border : solid 5px black;
-        height: 29%;
-        width: 80%;
-        margin: 8% 10% 0% 10%;
-    }
+.globalChan {
+  height: 29%;
+  width: 80%;
+  margin: 8% 10% 0% 10%;
+}
 
-    .sub {
-        height: 15%;
-    }
+.vid {
+  height: 100%;
+  width: auto;
+}
 
+.sub {
+  height: 15%;
+}
 </style>
--- wtv_interface/src/states/myChannelState.js
+++ wtv_interface/src/states/myChannelState.js
@@ -6,32 +6,38 @@ export const myChannelState =new Vue({
             {
                 'title': 'M6',
                 'img': 'logom6.png',
-                'id': 12
+                'id': 12,
+                'video': 'video1.mp4'
             },
             {
                 'title': 'France 2',
                 'img': 'france2.jpg',
-                'id': 120
+                'id': 120,
+                'video': 'video1.mp4'
             },
             {
                 'title': 'Arte',
                 'img': 'logow9.png',
-                'id': 129
+                'id': 129,
+                'video': 'video1.mp4'
             },
             {
                 'title': 'TF1',
                 'img': 'TF1.png',
-                'id': 110
+                'id': 110,
+                'video': 'video1.mp4'
             },
             {
                 'title': 'RMC',
                 'img': 'rmc.png',
-                'id': 132
+                'id': 132,
+                'video': 'video1.mp4'
             },
             {
                 'title': 'Arte',
                 'img': 'logow9.png',
-                'id': 912
+                'id': 912,
+                'video': 'video1.mp4'
             },
         ]
     },
