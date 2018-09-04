94163617c4267e2c137e814801093fe49eb39db0
maxoon <desfouxmaxime@gmail.com>
2018-09-04 14:02:13 +0200

    fix : remove listener menu

--- wtv_interface/src/components/menus/Menu.vue
+++ wtv_interface/src/components/menus/Menu.vue
@@ -2,20 +2,18 @@
     <div id="main-menu">
         <transition-group name="slide" tag="ul">
             <li class="menu-page" ref="contents"
-            v-for="(page, index) in pages" 
-            :key="page">{{page}}</li>
+                v-for="(page, index) in pages"
+                :key="page">{{page}}
+            </li>
         </transition-group>
         <div id="focusDiv"></div>
     </div>
 </template>
 
 <script>
-     import {mixinEltWithChild} from '../../mixins/mixinEltWithChild';
 
     export default {
-        components: {
-     
-        },
+        components: {},
         data() {
             return {
                 pages: ['HOME', 'TV', 'GUIDE', 'MEDIA CENTER', 'VOD', 'SETTINGS', 'SEARCH'],
@@ -24,19 +22,31 @@
         },
         methods: {
             ///----------Méthodes Animation-------------///
-            next () {
-                console.log(this.$refs.contents[this.focus])
+            next() {
                 const first = this.pages.shift()
                 this.pages = this.pages.concat(first)
-                },
-            previous () {   
-                console.log(this.$refs.contents[this.focus])
+            },
+            previous() {
                 const last = this.pages.pop()
-                this.pages = [last].concat(this.pages) 
-            }
-
-            ,initListeners: function () {
-                window.addEventListener('keydown', this.listener );
+                this.pages = [last].concat(this.pages)
+            },
+            isFocus() {
+                this.initListeners();
+            },
+            removeFocus() {
+                window.removeEventListener('keydown', this.listener);
+            },
+            setFocus: function (pos) {
+                if (this.focus <= 0 && pos == -1) {
+                    this.focus = this.$refs.contents.length - 1;
+                } else if (this.focus == this.$refs.contents.length - 1 && pos == 1) {
+                    this.focus = 0;
+                } else {
+                    this.focus += pos;
+                }
+            },
+            initListeners: function () {
+                window.addEventListener('keydown', this.listener);
             },
             listener: function ({code}) {
                 {
@@ -53,34 +63,21 @@
                     }
                 }
             },
-             setFocus: function (pos) {
-            if(this.focus <= 0 && pos == -1){
-                this.focus = this.$refs.contents.length-1;
-            }else if(this.focus == this.$refs.contents.length-1 && pos == 1){
-                this.focus = 0;
-            }else{
-                this.focus += pos;
-            }
-           
+
+            //     ///----------Fin Méthodes Navigation------///
+            // },
         },
-        //     ///----------Fin Méthodes Navigation------///
-        // },
-        },
-        mounted() {
-           this.initListeners();
-        }
-       
     }
 </script>
 
 <style scoped>
-    #main-menu{
+    #main-menu {
         display: flex;
         align-items: center;
         flex-direction: column;
         height: 10%;
         border: 1px solid grey;
-        padding:0; 
+        padding: 0;
     }
 
     #main-menu ul {
@@ -95,7 +92,7 @@
         box-sizing: border-box;
         display: flex;
         align-items: center;
-        justify-content: center;  
+        justify-content: center;
         flex: 0 0 10em;
         list-style: none;
         text-decoration: none;
@@ -103,9 +100,9 @@
         font-size: 20px;
         color: salmon;
         cursor: pointer;
-        left: 50%; 
+        left: 50%;
         right: 20%;
-        transition: transform 1s; 
+        transition: transform 1s;
     }
 
     li.menu-page[data-v-664b5cca] {
@@ -117,19 +114,19 @@
     }
 
     #focusDiv {
-        -webkit-box-shadow: inset -1px -1px 5px 12px rgba(255,255,255,1);
-        -moz-box-shadow: inset -1px -1px 5px 12px rgba(255,255,255,1);
-        box-shadow: inset -1px -1px 5px 5px rgba(255,255,255,1);
-        opacity:1;
+        -webkit-box-shadow: inset -1px -1px 5px 12px rgba(255, 255, 255, 1);
+        -moz-box-shadow: inset -1px -1px 5px 12px rgba(255, 255, 255, 1);
+        box-shadow: inset -1px -1px 5px 5px rgba(255, 255, 255, 1);
+        opacity: 1;
         border-radius: 5px;
-        padding:10px;
-        width:10%;
+        padding: 10px;
+        width: 10%;
         height: 7%;
         position: absolute;
     }
 
     li.menu-page:nth-child(4) {
-        opacity:1;
+        opacity: 1;
     }
 
 
